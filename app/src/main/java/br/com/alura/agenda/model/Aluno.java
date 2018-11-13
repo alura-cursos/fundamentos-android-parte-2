5b75f67cc8cb3fabfcc1a14da9d6f177c70f8656
Alex Felipe <alexfelipevieira@gmail.com>
2018-11-13 09:01:28 -0200

    projeto inicial

---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda.model;
+
+import android.support.annotation.NonNull;
+
+public class Aluno {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
