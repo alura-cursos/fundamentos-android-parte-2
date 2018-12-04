d26d0782a6c794971a7c462a21504bb39f40652f
Alex Felipe <alexfelipevieira@gmail.com>
2018-12-04 10:41:04 -0200

    vídeo 2.1

--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -8,9 +8,26 @@ import br.com.alura.agenda.model.Aluno;
 public class AlunoDAO {
 
     private final static List<Aluno> alunos = new ArrayList<>();
+    private static int contadorDeIds = 1;
 
     public void salva(Aluno aluno) {
+        aluno.setId(contadorDeIds);
         alunos.add(aluno);
+        contadorDeIds++;
+    }
+
+    public void edita(Aluno aluno) {
+        Aluno alunoEncontrado = null;
+        for (Aluno a :
+                alunos) {
+            if (a.getId() == aluno.getId()) {
+                alunoEncontrado = a;
+            }
+        }
+        if (alunoEncontrado != null) {
+            int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
+            alunos.set(posicaoDoAluno, aluno);
+        }
     }
 
     public List<Aluno> todos() {
