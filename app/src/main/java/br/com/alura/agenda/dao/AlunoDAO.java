f8606aad72cc4e2a08303f54ee8889099a967799
Alex Felipe <alexfelipevieira@gmail.com>
2018-12-05 09:24:46 -0200

    vídeo 3.1

--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -44,4 +44,11 @@ public class AlunoDAO {
     public List<Aluno> todos() {
         return new ArrayList<>(alunos);
     }
+
+    public void remove(Aluno aluno) {
+        Aluno alunoDevolvido = buscaAlunoPeloId(aluno);
+        if(alunoDevolvido != null){
+            alunos.remove(alunoDevolvido);
+        }
+    }
 }
