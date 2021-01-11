aba3a1c91e7518bc7b7f012efc7dda6c095ba31a
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-11 11:07:04 -0300

    atualizando aula

--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,11 +2,9 @@ package br.com.alura.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 
 import br.com.alura.agenda.R;
